fee830f37c5ab0961b2f38586f14e0a02605149f
ShubhamRaskar <shubhamraskar69@gmail.com>
2024-02-20 10:56:51 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'twubric';
+  filter: boolean = false;
+  httpClient = inject(HttpClient);
+  users: any = [];
+  filteredUsers: any = [];
+  sortBy: string = 'score';
+  startDate: Date | null = null;
+  endDate: Date | null = null;
+  searchData: string = '';
+
+  ngOnInit(): void {
+    this.httpClient
+      .get(
+        'https://gist.githubusercontent.com/pandemonia/21703a6a303e0487a73b2610c8db41ab/raw/82e3ef99cde5b6e313922a5ccce7f38e17f790ac/twubric.json'
+      )
+      .subscribe((data) => {
+        this.users = data;
+        this.filteredUsers = JSON.parse(JSON.stringify(this.users));
+      });
+  }
+
+filterDate(sDate: any, eDate: any) {
+  if (sDate && eDate) {
+    const startDate = new Date(sDate);
+    const endDate = new Date(eDate);
+    this.filteredUsers = this.users.filter((user: any) => {
+      const userJoinDate = new Date(user.join_date);
+      return (!startDate || userJoinDate >= startDate) && (!endDate || userJoinDate <= endDate);
+    });
+  }
+}
+
+sortByCriteria() {
+    this.filteredUsers = this.users.slice().sort((a: any, b: any) => {
+        return a.twubric[this.sortBy] - b.twubric[this.sortBy];
+    });
+}
+
+sortUser() {
+  this.filteredUsers = this.users.sort((a: any, b: any) => {
+    return a.username.localeCompare(b.username);
+  });
+}
+
+searchUser() {
+    const searchDataLowerCase = this.searchData.toLowerCase();
+    if (!searchDataLowerCase) {
+        this.filteredUsers = this.users;
+        return;
+    }
+    this.filteredUsers = this.users.filter((user: any) => {
+        return user.username
+            .toLowerCase()
+            .includes(searchDataLowerCase);
+    });
+}
+
+  removeFollower(i: any) {
+    this.filteredUsers.splice(i, 1);
+  }
+
+resetFilter(type: any) {
+    if (type == 'date') {
+      this.startDate = null;
+      this.endDate = null;
+    }
+    if (type == 'criteria') {
+      this.sortBy = 'score';
+    }
+    if (type == 'all') {
+      this.startDate = null;
+      this.endDate = null;
+      this.sortBy = 'score';
+      this.filter = false;
+    }
+    this.filteredUsers = this.users;
+  }
 }
